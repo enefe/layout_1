b5d7028c0bc1567b76c9d6d9bd4aeaed168cfa7c
enefe815 <enefe815@yandex.ru>
2022-05-27 17:56:31 +0300

    fix: slider js productions 2

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ let isDragging = false,
 slider.addEventListener('touchstart', touchStart());
 slider.addEventListener('touchend', touchEnd);
 slider.addEventListener('touchmove', touchMove);
+slider.addEventListener('touchmove', function(event) { event.preventDefault(); }, false);
 slider.addEventListener('mousedown', touchStart());
 slider.addEventListener('mouseup', touchEnd);
 slider.addEventListener('mousemove', touchMove);
@@ -39,51 +40,27 @@ function touchStart() {
     }
 }
 
-/* let limits = {
-    top: big.offsetTop,
-    right: big.offsetWidth + big.offsetLeft - slider.offsetWidth,
-    bottom: big.offsetHeight + big.offsetTop - slider.offsetHeight,
-    left: big.offsetLeft
-}; */
-
 function touchMove(event) {
     if (isDragging) {
         const currentPosition = getPositionX(event)
-            /* console.log(currentPosition); */
         currentTranslate = prevTranslate + currentPosition - startPos;
+        /* console.log(window.innerWidth - slider.offsetWidth)
+        console.log(currentTranslate + 300) */
         if (currentTranslate > 0) {
             currentTranslate = 0;
         }
-        /* let newLocation = {
-            x: limits.left,
-            y: limits.top
-        };
-        if (event.pageX > limits.right) {
-            newLocation.x = limits.right;
-        } else if (event.pageX > limits.left) {
-            newLocation.x = event.pageX;
+        if (window.innerWidth - slider.offsetWidth > currentTranslate + 300) {
+            currentTranslate = window.innerWidth - slider.offsetWidth;
         }
-        if (event.pageY > limits.bottom) {
-            newLocation.y = limits.bottom;
-        } else if (event.pageY > limits.top) {
-            newLocation.y = event.pageY;
-        } */
     }
 }
 
 function touchEnd() {
+
     cancelAnimationFrame(animationID);
     isDragging = false;
     setPositionByIndex();
     slider.classList.remove('grabbing');
-
-    const movedBy = currentTranslate - prevTranslate
-
-    // if moved enough negative then snap to next slide if there is one
-    if (movedBy < -100 && currentIndex < slides.length - 1) currentIndex += 1
-
-    // if moved enough positive then snap to previous slide if there is one
-    if (movedBy > 100 && currentIndex > 0) currentIndex -= 1
 
     setPositionByIndex()
 }
